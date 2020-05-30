adfca83571aaf18ce235d20d5bc9a81861fbd395
Daniel <eoyeniran@tradedept.co>
2020-05-30 19:46:42 +0100

    build app

---
+++ build/precache-manifest.1441837e758e1732da47f39361b07e5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "345849cb1af48dc4ff3485e440e560e8",
+    "url": "/static/media/pic-1.345849cb.png"
+  },
+  {
+    "revision": "bd25567e79bfb28da72bfafa611ebc0f",
+    "url": "/static/media/logo.bd25567e.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0a9e098b5e3ae0158392",
+    "url": "/static/js/main.362be852.chunk.js"
+  },
+  {
+    "revision": "5b6af2dd5fdf9dc557fa",
+    "url": "/static/js/2.83e93d69.chunk.js"
+  },
+  {
+    "revision": "0a9e098b5e3ae0158392",
+    "url": "/static/css/main.3a0d36f2.chunk.css"
+  },
+  {
+    "revision": "7e125da7ece7e53783025a6937bec820",
+    "url": "/index.html"
+  }
+];
