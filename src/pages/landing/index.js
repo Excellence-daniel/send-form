6b06688477a83f57b91d69f22635a5b622b6dcb5
Daniel <eoyeniran@tradedept.co>
2020-05-30 08:51:15 +0100

    made the eqipments to be a component

--- src/pages/landing/index.js
+++ src/pages/landing/index.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import Header from '../../components/main-page-header';
 import PictureOne from './images/pic-1.png';
 import './landing.scss';
+import Equipments from '../../components/equipments';
 
 export default function LandingPage() {
 	return (
@@ -89,73 +90,12 @@ export default function LandingPage() {
 						<h4>
 							We're working with manufacturers producing <br /> millions of high demand products per week.
 						</h4>
-						<div className="equipment-types">
-							<div className="equipment">
-								<img
-									src="https://uploads-ssl.webflow.com/5a7269a5582f52000102d51d/5e9da596b99e412007923c2e_N95.svg"
-									alt="A Respirator"
-									class="covid-type-icon"
-								/>
-								<p>Respirators</p>
-							</div>
-							<div className="equipment">
-								<img
-									src="https://uploads-ssl.webflow.com/5a7269a5582f52000102d51d/5e9d685e38205a2f86da9be8_Glove.svg"
-									alt="A Respirator"
-									class="covid-type-icon"
-								/>
-								<p>Gloves</p>
-							</div>
-							<div className="equipment">
-								<img
-									src="https://uploads-ssl.webflow.com/5a7269a5582f52000102d51d/5e9d685e38205ac3e6da9be7_Mask.svg"
-									alt="A Respirator"
-									class="covid-type-icon"
-								/>
-								<p>Face Shields</p>
-							</div>
-							<div className="equipment">
-								<img
-									src="https://uploads-ssl.webflow.com/5a7269a5582f52000102d51d/5e9d685e1ceb3e5c17649a7d_Sanitizer.svg"
-									alt="A Respirator"
-									class="covid-type-icon"
-								/>
-								<p>Sanitizer</p>
-							</div>
-							<div className="equipment">
-								<img
-									src="https://uploads-ssl.webflow.com/5a7269a5582f52000102d51d/5e9d69b91ceb3e7c8164a18a_Respirator.svg"
-									alt="A Respirator"
-									class="covid-type-icon"
-								/>
-								<p>Ventilators</p>
-							</div>
-							<div className="equipment">
-								<img
-									src="https://uploads-ssl.webflow.com/5a7269a5582f52000102d51d/5e9d685ee014f924d6aad1b6_SurgicalMask.svg"
-									alt="A Respirator"
-									class="covid-type-icon"
-								/>
-								<p>Masks</p>
-							</div>
-							<div className="equipment">
-								<img
-									src="https://uploads-ssl.webflow.com/5a7269a5582f52000102d51d/5e9d685e38205ab232da9be4_Wipes.svg"
-									alt="A Respirator"
-									class="covid-type-icon"
-								/>
-								<p>Wipes</p>
-							</div>
-							<div className="equipment">
-								<img
-									src="https://uploads-ssl.webflow.com/5a7269a5582f52000102d51d/5e9d685d9aae7e0b16bf20be_Gown.svg"
-									alt="A Respirator"
-									class="covid-type-icon"
-								/>
-								<p>Gowns</p>
-							</div>
+						<>
+							<Equipments />
+						</>
+						<div className="coming-soon-equipments">
+							<button> 25+ More Classes of Goods Coming Soon</button>
 						</div>
-						<div className="coming-soon-eqipments"></div>
 					</div>
 				</div>
 			</div>
